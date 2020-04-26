750832bb5a3427ddba29ec45dd75815eed44ddf7
Jack Comer <5665759+Strid3r2442@users.noreply.github.com>
2020-04-26 13:54:22 +0100

    Added basic !play functionality accepting url input

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,6 @@
 import Player from './player';
+import Transcoder from './transcoder';
+
 import Mumble = require('mumble');
 import { PathLike } from 'fs';
 
@@ -6,7 +8,9 @@ export default class Client {
 	private readonly _username: string;
 	private readonly _comment: string;
 	private readonly _options: Mumble.Options;
+	private _connection: Mumble.Connection;
 	private _player: Player;
+	private _transcoder: Transcoder;
 
 	constructor(username: string, comment?: string, options?: Mumble.Options) {
 		this._username = username;
@@ -28,8 +32,16 @@ export default class Client {
 
 			connection.authenticate(this._username, password);
 			connection.on('initialized', () => {
-				this._player = new Player(connection);
-				callback(connection);
+				this._connection = connection;
+				this._player = new Player(this._connection);
+				this._transcoder = new Transcoder(this);
+				callback(this._connection);
+			})
+			connection.on('message', (msg, user) => {
+				if(msg.startsWith('!')) {
+					const cmd = msg.substring(1);
+					return this._onMessage(cmd, user)
+				}
 			})
 		});
 	}
@@ -40,5 +52,48 @@ export default class Client {
 	 */
 	public playFile(filename: PathLike) {
 		this._player.playFile(filename);
+	}
+
+	/**
+	 * Send a message to the current channel
+	 * @param msg Message to be sent
+	 */
+	public sendMessage(msg: string) {
+		const channel = this._connection.user.channel;
+		channel.sendMessage(msg);
+	}
+
+	/**
+	 * Message listener
+	 * @param msg The message received
+	 * @param user User that sent the message
+	 */
+	private async _onMessage(msg: string, user: Mumble.User) {
+		const channel = this._connection.user.channel;
+		// Strip HTML tags from message
+		msg = msg.replace(/(<([^>]+)>)/ig,"");
+
+		const cmd = msg.split(' ')[0];
+		const args = msg.split(' ').slice(1);
+
+		switch(cmd) {
+			case 'play':
+				if(args.length > 1) {
+					channel.sendMessage('Invalid arguments');
+					break;
+				}
+
+				try {
+					const filePath = await this._transcoder.download(args[0]);
+					this.playFile(filePath);
+				} catch(e) {
+					console.log(e);
+					channel.sendMessage('An error occured whilst attempting to download file')
+				}
+				break;
+				
+			default:
+				channel.sendMessage('Command not found');
+		}
 	}
 }
--- src/transcoder.ts
+++ src/transcoder.ts
@@ -1,10 +1,15 @@
+import Client from './client';
+
 import ytdl = require('ytdl-core');
 import ffmpeg = require('fluent-ffmpeg');
 
+
 export default class Transcoder {
+	private readonly _client: Client;
 	private readonly _options: ytdl.downloadOptions;
 
-	constructor(options?: ytdl.downloadOptions) {
+	constructor(client: Client, options?: ytdl.downloadOptions) {
+		this._client =  client;
 		this._options = options;
 	}
 
@@ -12,17 +17,30 @@ export default class Transcoder {
 	 * Download audio from internet source
 	 * @param url URL to audio
 	 */
-	public download(url: string) {
+	public async download(url: string) {
+		const info = ytdl.getBasicInfo(url, (err, info) => {
+			if(err) {
+				throw err;
+			}
+
+			this._client.sendMessage(`Download of <b>${info.title}</b> started...`);
+			return info;
+		})
+		const filePath = `./audio/${(await info).video_id}.mp3`;
+
 		const stream = ytdl(url, {
 			quality: 'highestaudio'
 		});
 
-		ffmpeg(stream)
-			.audioBitrate(128)
-			.save('./audio/test.mp3')
-			.on('end', () => {
-				console.log('done')
-			})
+		return new Promise<string>(resolve => {
+			ffmpeg(stream)
+				.audioBitrate(128)
+				.save(filePath)
+				.on('end', () => {
+					resolve(filePath);
+				})
+		})
+			
 
 	}
 }
